f561e0568dd882957292b511f3d7659428f78036
tayou89 <ysh00718@gmail.com>
2023-03-20 06:40:22 +0900

    23/03/20 06:40

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tayou <tayou@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:21:31 by tayou             #+#    #+#             */
-/*   Updated: 2023/03/19 17:24:03 by tayou            ###   ########.fr       */
+/*   Updated: 2023/03/20 06:40:05 by tayou            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,74 +90,89 @@ int	check_perfectly_sorted_stack_b(t_node *stack)
 	return (1);
 }
 
-void	check_both_stack(t_node *stack_a, t_node *stack_b)
+int	check_stack_a(t_node **stack_a, t_node **stack_b)
 {
-	if (stack_a->number > stack_a->next->number &&
-		stack_b->number < stack_b->next->number)
+	int	max_number;
+	int	min_number;
+
+	max_number = get_max_number(*stack_a);
+	min_number = get_min_number(*stack_a);
+	if ((*stack_a)->number == max_number &&
+		(*stack_a)->next->number == min_number)
 	{
-		command_swap(stack_a);
-		command_swap(stack_b);
+		command_rotate_up(stack_a, stack_b);
+		ft_printf("ra\n");
+	}
+	if ((*stack_a)->number > (*stack_a)->next->number)
+		return (1);
+	return (0);
+}
+
+int	check_stack_b(t_node **stack_b, t_node **stack_a)
+{
+	int	max_number;
+	int	min_number;
+
+	max_number = get_max_number(*stack_b);
+	min_number = get_min_number(*stack_b);
+	if ((*stack_b)->number == min_number &&
+		(*stack_b)->next->number == max_number)
+	{
+		command_rotate_up(stack_b, stack_a);
+		ft_printf("rb\n");
+	}
+	if ((*stack_b)->number < (*stack_b)->next->number)
+		return (1);
+	return (0);
+}
+
+void	check_both_stack(t_node **stack_a, t_node **stack_b)
+{
+	if (check_stack_a(stack_a, stack_b) == 1 &&
+		check_stack_b(stack_b, stack_a) == 1)
+	{
+		command_swap(*stack_a);
+		command_swap(*stack_b);
 		ft_printf("ss\n");
-		return ;
 	}
-	if (stack_a->number > stack_a->next->number &&
-		stack_b->number > stack_b->next->number)
+	if (check_stack_a(stack_a, stack_b) == 1 &&
+		check_stack_b(stack_b, stack_a) != 1)
 	{
-		command_swap(stack_a);
-		ft_printf("sa\n");
-		return ;
-	}
-	if (stack_a->number < stack_a->next->number &&
-		stack_b->number < stack_b->next->number)
-	{
-		command_swap(stack_b);
-		ft_printf("sb\n");
-		return ;
-	}
-	else
-		return ;
-}
-
-void	check_stack_a(t_node *stack_a)
-{
-	if (stack_a->number > stack_a->next->number)
-	{
-		command_swap(stack_a);
+		command_swap(*stack_a);
 		ft_printf("sa\n");
 	}
-	return ;
-}
-
-void	check_stack_b(t_node *stack_b)
-{
-	if (stack_b->number < stack_b->next->number)
+	if (check_stack_a(stack_a, stack_b) != 1 &&
+		check_stack_b(stack_b, stack_a) == 1)
 	{
-		command_swap(stack_b);
+		command_swap(*stack_b);
 		ft_printf("sb\n");
 	}
 }
 
-void	check_first_second(t_node *stack_a, t_node *stack_b)
+void	check_first_second(t_node **stack_a, t_node **stack_b)
 {
 	int	stack_a_size;
 	int	stack_b_size;
 
-	stack_a_size = get_stack_size(stack_a);
-	stack_b_size = get_stack_size(stack_b);
+	stack_a_size = get_stack_size(*stack_a);
+	stack_b_size = get_stack_size(*stack_b);
 	if (stack_a_size >= 2 && stack_b_size >= 2)
-	{
 		check_both_stack(stack_a, stack_b);
-		return ;
-	}
-	if (stack_a_size >= 2 && stack_b_size < 2)
+	else if (stack_a_size >= 2 && stack_b_size < 2)
 	{
-		check_stack_a(stack_a);
-		return ;
+		if (check_stack_a(stack_a, stack_b) == 1)
+		{
+			command_swap(*stack_a);
+			ft_printf("sa\n");
+		}
 	}
-	if (stack_a_size < 2 && stack_b_size >= 2)
+	else if (stack_a_size < 2 && stack_b_size >= 2)
 	{
-		check_stack_b(stack_b);
-		return ;
+		if (check_stack_b(stack_b, stack_a) == 1)
+		{
+			command_swap(*stack_b);
+			ft_printf("sb\n");
+		}
 	}
 }
 
@@ -231,6 +246,9 @@ void	sort_stack_b(t_node **stack_b, t_node **stack_a, int not_sorted_point)
 	}
 	command_swap(*stack_b);
 	ft_printf("sb\n");
+	not_sorted_point = get_not_sorted_point(*stack_b);
+	if (not_sorted_point > 0)
+		sort_stack_b(stack_b, stack_a, not_sorted_point);
 	i = 0;
 	while (i < not_sorted_point)
 	{
@@ -249,7 +267,6 @@ void	check_if_sorted_stack_b(t_node **stack_b, t_node **stack_a)
 	if (size < 3)
 		return ;
 	not_sorted_point = get_not_sorted_point(*stack_b);
-	ft_printf("not_sorted_point: %d\n", not_sorted_point);
 	if (not_sorted_point > 0)
 		sort_stack_b(stack_b, stack_a, not_sorted_point);
 }
@@ -282,7 +299,7 @@ void	put_max_on_top(t_node **stack_to_rotate, t_node **other_stack, int count)
 	}
 }
 
-void	check_max_number_index(t_node **stack_to_check, t_node **other_stack)
+void	check_max_number_is_top(t_node **stack_to_check, t_node **other_stack)
 {
 	int	stack_size;
 	int	max_number;
@@ -293,35 +310,99 @@ void	check_max_number_index(t_node **stack_to_check, t_node **other_stack)
 		return ;
 	max_number = get_max_number(*stack_to_check);
 	max_number_index = get_max_number_index(*stack_to_check, max_number);
-	ft_printf("max_number_index: %d\n", max_number_index);
 	if (max_number_index > 0)
 		put_max_on_top(stack_to_check, other_stack, max_number_index);
 }
 
+void	sort_until_sorted(t_node **stack_a, t_node **stack_b)
+{
+	int	i;
+
+	i = 0;
+	while (check_perfectly_sorted_stack_a(*stack_a) == 0 ||
+		   check_perfectly_sorted_stack_b(*stack_b) == 0)
+	{
+		check_first_second(stack_a, stack_b);
+		check_if_sorted_stack_b(stack_b, stack_a);
+		check_max_number_is_top(stack_b, stack_a);
+		check_first_last(stack_a, stack_b);
+		i++;
+	}
+}
+
+int	get_min_number_index(t_node *stack)
+{
+	int	min_number;
+	int	min_number_index;
+	int	i;
+
+	min_number = get_min_number(stack);
+	i = 0;
+	while (stack->number != min_number)
+	{
+		stack = stack->next;
+		i++;
+	}
+	min_number_index = i;
+	return (min_number_index);
+}
+
+void	put_min_on_top(t_node **stack_a, t_node **stack_b, int index, int point)
+{
+	int	min_number;
+
+	min_number = get_min_number(*stack_a);
+	if (index > point)
+	{
+		while ((*stack_a)->number != min_number)
+		{
+			command_rotate_down(stack_a, stack_b);
+			ft_printf("rra\n");
+		}
+	}
+	else
+	{
+		while ((*stack_a)->number != min_number)
+		{
+			command_rotate_up(stack_a, stack_b);
+			ft_printf("ra\n");
+		}
+	}
+}
+
+void	check_min_number_is_top(t_node **stack_a, t_node **stack_b)
+{
+	int	stack_size;
+	int	middle_point;
+	int	min_number_index;
+
+	stack_size = get_stack_size(*stack_a);
+	if (stack_size < 2)
+		return ;
+	middle_point = stack_size / 2;
+	min_number_index = get_min_number_index(*stack_a);
+	put_min_on_top(stack_a, stack_b, min_number_index, middle_point);
+}
+
+void	put_everynode_to_stack_a(t_node **stack_a, t_node **stack_b)
+{
+	while ((*stack_b) != (void *) 0)
+	{
+		command_push(stack_b, stack_a);
+		ft_printf("pa\n");
+	}
+}
 
 t_node	*sort_stack_a(t_node *stack_a)
 {
 	t_node	*stack_b;
-	int		i;
 
 	stack_b = (void *) 0;
 	if (check_perfectly_sorted_stack_a(stack_a) == 1)
 		return (stack_a);
-	i = 0;
-//	while (check_perfectly_sorted_stack_a(stack_a) == 0 ||
-//		   check_perfectly_sorted_stack_b(stack_b) == 0)
-	while (i < 7)
-	{
-		check_first_second(stack_a, stack_b);
-		check_if_sorted_stack_b(&stack_b, &stack_a);
-		check_max_number_index(&stack_b, &stack_a);
-		check_first_last(&stack_a, &stack_b);
-		ft_printf("------------stack_a--------------\n");
-		print_stack(stack_a);
-		ft_printf("------------stack_b--------------\n");
-		print_stack(stack_b);
-		i++;
-	}
+	sort_until_sorted(&stack_a, &stack_b);
+	check_min_number_is_top(&stack_a, &stack_b);
+	put_everynode_to_stack_a(&stack_a, &stack_b);
 	free_list(stack_b);
 	return (stack_a);
 }
@@ -332,10 +413,7 @@ int	main(int argc, char *argv[])
 
 	check_exception(argc, argv);
 	stack_a = get_stack_a(argv);
-	ft_printf("----------------original stack_a-----------------\n");
-	print_stack(stack_a);
 	stack_a = sort_stack_a(stack_a);
 	free_list(stack_a);
-	system("leaks push_swap");
 	return (0);
 }
